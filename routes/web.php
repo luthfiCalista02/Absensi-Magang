ba386a576e0c0a9faf87412510c0404a049ce099
luthfiCalista02 <luthficalista07@gmail.com>
2024-10-24 20:30:12 +0700

    bikin login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[HomeController::class,'dashboard']);
+Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::post('/login-proses',[LoginController::class,'login_proses'])->name('login-proses');
+
+Route::get('/dashboard',[HomeController::class,'dashboard'])->name('dashboard');
 
 Route::get('/user',[HomeController::class,'index'])->name('index');
 Route::get('/create',action: [HomeController::class,'create'])->name('user.create');
